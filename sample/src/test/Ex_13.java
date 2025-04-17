cbbaff1a62dc41facb0a2527936babd495150f89
YUJIN2758 <wlsdl2758@navercom>
2025-04-17 17:20:14 +0900

    수정이 되니?

--- sample/src/test/Ex_13.java
+++ sample/src/test/Ex_13.java
@@ -3,7 +3,7 @@ package test;
 import java.util.Arrays;
 
 /*문자열 before와 after가 매개변수로 주어질 때, 
-before의 순서를 바꾸어 after를 만들 수 있으면 1을, 만들 수 없으면 0을 return 하도록 solution 함수를 완성해보세요.*/
+before의 순서를 바꾸어 after를 만들 수 있으면 1을, 만들 수 없으면 0을 return 하도록 solution 함수를 완성해보세요.ㅁㅁㅁㅁㅁㅁ*/
 public class Ex_13 {
 
 	public static void main(String[] args) {
